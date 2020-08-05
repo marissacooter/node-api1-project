093f1e9f65e48e9804dead8180772691af88f12b
Marissa <marissacooterl@gmail.com>
2020-08-05 15:08:21 -0500

    get users done

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 let users = [
-	{ id: "1", name: "Jane Doe" },
-	{ id: "2", name: "John Doe" },
-	{ id: "3", name: "Jack Doe" },
+	{ id: "1", name: "Marissa" },
+	{ id: "2", name: "Lauren" },
+	{ id: "3", name: "Test" },
 ]
 
 function getUsers() {
--- server.js
+++ server.js
@@ -5,3 +5,16 @@ const server = express()
 
 server.use(express.json())
 
+server.get("/", (req, res) => {
+    res.json({ message: "Here's my Node API 1 Project :) "})
+})
+
+server.get("/users", (req, res) => {
+    const users = db.getUsers()
+
+    res.json(users)
+})
+
+server.listen(8080, () => {
+    console.log("[ SERVER RUNNING ON PORT 8080 ... ]")
+})
